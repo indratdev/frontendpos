53954df3733f9db4b2fa0925a48d13a36d460214
indratdev <indrat.dev@gmail.com>
2025-02-25 21:29:05 +0700

    initial-commit

---
+++ lib/config/routes.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+// import 'package:kita_muslim/presentation/screens/article_screen/article_screen.dart';
+// import 'package:kita_muslim/presentation/screens/calculator_screen/calculator_screen.dart';
+// import 'package:kita_muslim/presentation/screens/favorite_screens/favorite_screen.dart';
+// import 'package:kita_muslim/presentation/screens/hadist_screen/hadistSRscreen.dart';
+// import 'package:kita_muslim/presentation/screens/hadist_screen/hadist_detail_screen.dart';
+
+// import 'package:kita_muslim/presentation/screens/hadist_screen/hadistscreen.dart';
+// import 'package:kita_muslim/presentation/screens/home_screen/home_screen_v2.dart';
+// import 'package:kita_muslim/presentation/screens/prayer_schedule_screen/prayer_schedule_screen.dart';
+
+// import 'package:kita_muslim/presentation/screens/qiblah_screen/qiblah_screen.dart';
+// import 'package:kita_muslim/presentation/screens/quran_screen/quran_screen.dart';
+// import 'package:kita_muslim/presentation/screens/splash_screen/splashscreen.dart';
+// import 'package:kita_muslim/presentation/screens/surah_daily/surah_daily_screen.dart';
+// import 'package:kita_muslim/presentation/screens/surah_detail/surahdetail_screen.dart';
+// import 'package:kita_muslim/presentation/screens/surah_daily/surah_daily_detail_screen.dart';
+
+class Routes {
+  // static const String splash = "/splash";
+  // static const String homeScreen = "/homescreen";
+  // static const String readAlquran = "/bacaalquran";
+  // static const String detailSurah = "/surahdetail";
+  // static const String dailySurah = "/doaharian";
+  // static const String detailDailyPrayer = "/doahariandetail";
+  // static const String favoriteSurah = "/favoriteSurah";
+  // static const String hadists = "/hadists";
+  // static const String hadistDetail = "/hadistdetail";
+  // static const String hadistsSR = "/hadistsSR";
+  // static const String prayerSchedule = "/prayerschedule";
+  // static const String qiblahScreen = "/qiblahscreen";
+  // static const String calculatorScreen = "/calculatorscreen";
+  // static const String articleScreen = "/articlescreen";
+
+  // Map<String, WidgetBuilder> getRoutes = {
+  //   homeScreen: (_) => HomeScreenV2(),
+  //   readAlquran: (_) => QuranScreen(),
+  //   detailSurah: (_) => SurahDetailScreen(),
+  //   dailySurah: (_) => SurahDailyScreen(),
+  //   detailDailyPrayer: (_) => SurahDailyDetailScreen(),
+  //   favoriteSurah: (_) => const FavoriteScreen(),
+  //   hadists: (_) => HadistScreen(),
+  //   hadistDetail: (_) => HadistDetailScreen(nameHadist: ""),
+  //   // hadistDetail: (_) => HadistDetailScreen(),
+  //   hadistsSR: (_) => HadistSRScreen(),
+  //   splash: (_) => const SplashScreen(),
+  //   prayerSchedule: (_) => const PrayerScheduleScreen(),
+  //   qiblahScreen: (_) => QiblahScreen(),
+  //   calculatorScreen: (_) => CalculatorScreen(),
+  //   articleScreen: (_) => ArticleScreen(),
+  // };
+}
