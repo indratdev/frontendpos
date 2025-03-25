4723d60dd0861104e0aa99a5400881b508e5f3c6
indratdev <indrat.dev@gmail.com>
2025-03-25 19:42:26 +0700

    update

--- lib/presentation/screens/master_screen/master_screen.dart
+++ lib/presentation/screens/master_screen/master_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontendpos/config/routes.dart';
 import 'package:frontendpos/shared/utils/constants.dart';
 import 'package:frontendpos/shared/utils/export.dart';
 
@@ -11,16 +12,21 @@ class MasterScreen extends StatelessWidget {
       MenuMasterWidget(
           title: "Master 1",
           description: "description",
-          routeName: "null",
+          routeName: Routes.editMaster,
           icon: "icon"),
       MenuMasterWidget(
           title: "Master 2",
           description: "description",
-          routeName: "null",
-          icon: "icon"),
+          routeName: Routes.editMaster,
+          icon: null),
     ];
 
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        elevation: 5,
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: Text("Master"),
         centerTitle: true,
@@ -46,14 +52,14 @@ class MenuMasterWidget extends StatelessWidget {
   final String title;
   final String description;
   final String routeName;
-  final String icon;
+  final String? icon;
 
   const MenuMasterWidget({
     super.key,
     required this.title,
     required this.description,
     required this.routeName,
-    required this.icon,
+    this.icon,
   });
 
   @override
@@ -69,36 +75,40 @@ class MenuMasterWidget extends StatelessWidget {
         height: MediaQuery.sizeOf(context).height / 5,
         child: Row(
           children: [
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 8),
-              width: MediaQuery.sizeOf(context).width / 5,
-              child: ClipOval(
-                child: Image.asset(
-                  Constants.allIcon,
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) {
-                    return Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.red[300],
-                        shape: BoxShape.circle,
+            (icon == null)
+                ? SizedBox()
+                : Container(
+                    margin: EdgeInsets.symmetric(horizontal: 8),
+                    width: MediaQuery.sizeOf(context).width / 5,
+                    child: ClipOval(
+                      child: Image.asset(
+                        Constants.allIcon,
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.red[300],
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(
+                              Icons.person,
+                              size: 100 * 0.5,
+                              color: Colors.white,
+                            ),
+                          );
+                        },
                       ),
-                      child: Icon(
-                        Icons.person,
-                        size: 100 * 0.5,
-                        color: Colors.white,
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
+                    ),
+                  ),
             Expanded(
               child: Container(
-                margin: EdgeInsets.fromLTRB(0, 18, 8, 8),
+                margin: (icon == null)
+                    ? EdgeInsets.fromLTRB(18, 18, 8, 8)
+                    : EdgeInsets.fromLTRB(0, 18, 8, 8),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
